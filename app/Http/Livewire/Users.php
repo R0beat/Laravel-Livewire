22cfcf64c7649b22d3c3180e6c31784e1f192bb2
Roberto Gonzalez Arriaga <htk.rgl@gmail.com>
2021-08-12 23:01:24 -0500

    Desplegando Usuarios

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -1,13 +1,39 @@
 <?php
 
 namespace App\Http\Livewire;
-
+#Nos Conectamos a nuestra entidad de Usuarios
+use App\Models\User;
 use Livewire\Component;
 
 class Users extends Component
 {
-    public function render()
-    {
-        return view('livewire.users');
+    public $name, $user, $password,$email;
+
+    public function render(){
+        return view('livewire.users',[
+            'users' => User::latest()->get()
+        ]);
+    }
+    public function store(){
+        $this-> validate([
+            'name' => 'required',
+            'password' => 'required',
+            'email' => 'required',
+        ]);
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            # Usamos el helper para encyptar las contraseñas
+            'password' => bcrypt($this->password),
+
+        ]);
+        
+        # Despues del regstro vaciamos las propiedades
+        $this->name = '';
+        $this->email = '';
+        $this->password = '';
+
+        # Mensaje de Sesión que se enviara a nuestra vista
+        session()->flash('message','Usuario Creado');
     }
 }
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,3 +1,39 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    @if (session()->has('message'))
+    {{ session('message') }}
+    @endif
+    <form action="">
+        <label for="">Nombre:</label>
+        <input type="text" wire:model='name'>
+        @error('name') <em> {{ $message }}</em> @enderror <br>
+        <label for="">Email:</label>
+        <input type="text" wire:model='email'>
+        @error('email') <em> {{ $message }}</em> @enderror <br>
+        <label for="">Password:</label>
+        <input type="text" wire:model='password'>
+        @error('password') <em> {{ $message }}</em> @enderror <br>
+
+        <button wire:click.prevent='store()'>Guardar</button>
+    </form>
+    <hr>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user-> name}}</td>
+                        <td>{{ $user-> email}}</td>
+                    </tr>
+                @endforeach
+            </tr>
+        </tbody>
+    </table>
 </div>
